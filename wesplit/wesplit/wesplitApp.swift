94ab4172d7b13bce74d5ba3785f35094b7522692
jorgemhtdev <jorgemht@outlook.es>
2020-11-29 22:09:23 +0100

    Project 1, part one Project 1, part two Project 1, part three

---
+++ wesplit/wesplit/wesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  wesplitApp.swift
+//  wesplit
+//
+//  Created by jorgemhtdev on 29/11/20.
+//
+
+import SwiftUI
+
+@main
+struct wesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
